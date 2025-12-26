a81426a030ac7cfab19f83e29d07ee9dd8b27e99
rrrrrzy <rzy1282950305@163.com>
2025-12-26 16:25:28 +0800

    fix: daily_mission's api key

--- daily_mission.sh
+++ daily_mission.sh
@@ -16,6 +16,7 @@ ADB_EXEC="/opt/homebrew/bin/adb" # 或者 /opt/homebrew/bin/adb
 # 4. 设置环境变量 (连接你的本地 Ollama)
 # export OPENAI_API_BASE="https://ollama.edulearn.cn/v1"
 # export OPENAI_API_KEY="rrrrrzy"
+export GLM_KEY
 
 # =======================================================
 
@@ -23,6 +24,15 @@ ADB_EXEC="/opt/homebrew/bin/adb" # 或者 /opt/homebrew/bin/adb
 cd "$PROJECT_DIR" || exit
 
 echo "[$(date)] 开始执行每日任务..." #>> run.log
+
+if [ -f .env ]; then
+    echo "正在加载 .env 配置..." >> run.log
+    # source命令会读取文件并执行里面的 export 语句
+    source .env
+else
+    echo "错误: 找不到 .env 文件！任务终止。" >> run.log
+    exit 1
+fi
 
 # 1. 唤醒手机 (防止黑屏无法操作)
 # 模拟按下电源键唤醒
@@ -41,12 +51,17 @@ sleep 2
 #     --model "hf.co/mradermacher/AutoGLM-Phone-9B-GGUF:Q8_0" \
 #     "打开网易云音乐并播放每日推荐" >> run.log 2>&1
 "$PYTHON_EXEC" main.py \
-    --base-url https://open.bigmodel.cn/api/paas/v4 --model "autoglm-phone" --apikey "REMOVED_KEY \
-    "步骤1：打开米游社，进入原神频道，点击签到福利
-    步骤2：如果打开时就**没有**右上角显示红点的卡片，说明**不需要再签到了**，直接进入步骤5
+    --base-url https://open.bigmodel.cn/api/paas/v4 --model "autoglm-phone" --apikey "$GLM_KEY" \
+    "[执行步骤]
+    步骤1：打开米游社，进入原神频道，点击签到福利
+    步骤2：如果已经签过到，直接进入步骤5
     步骤3：点击右上角有红点的卡片图标，完成每日签到
     步骤4：签完后你需要重复确认，是否还存在有红点的卡片，如果还有，说明签到失败，需要重试；
-    步骤5：最后，请把手机返回到桌面。" \
+    步骤5：最后，请把手机返回到桌面。
+    [提示]
+    1.如何判断已经签到：如果有「对号」的卡片的**下一个卡片**右上方没有红点，说明今天已经完成签到了
+    2.禁止补签，签到日期不一定和卡片上的时间对应
+    " \
     >> run.log 2>&1
 
 # 再次用 adb 结束米游社进程
